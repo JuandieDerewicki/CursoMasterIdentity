5ea5a2e76ca1935194fb94abce69cce5667aec80
JuandieDerewicki <jdderewicki@gmail.com>
2024-03-07 20:50:03 -0300

    Creamos el controlador llamado CuentasController y la vista del Registro de usuario, ademas modificamos el layout para agregar el boton del registro

---
+++ Controllers/CuentasController.cs
@@ -0,0 +1,20 @@
+﻿using CursoIdentityUdemy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoIdentityUdemy.Controllers
+{
+    public class CuentasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Registro()
+        {
+            RegistrerViewModel registroVM = new RegistrerViewModel();   
+            return View(registroVM);    
+        }
+    }
+}
--- obj/Debug/net6.0/CursoIdentityUdemy.AssemblyInfo.cs
+++ obj/Debug/net6.0/CursoIdentityUdemy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoIdentityUdemy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e539acee74403553f0262a3b7559a9c1ad212432")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbf1bd2701d5c2f8594df0d1419a58d9c8d45ff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoIdentityUdemy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoIdentityUdemy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
