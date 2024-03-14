828aeede34d34bb82d771858d51835dfe420b660
JuandieDerewicki <jdderewicki@gmail.com>
2024-03-14 11:37:11 -0300

    Creamos la vista de ConfirmarEmail

--- obj/Debug/net6.0/CursoIdentityUdemy.AssemblyInfo.cs
+++ obj/Debug/net6.0/CursoIdentityUdemy.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoIdentityUdemy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+985a48087466c8f8f497541e13bb70935db03207")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85fc8f991a32030726f494329be1270193037257")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoIdentityUdemy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoIdentityUdemy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
