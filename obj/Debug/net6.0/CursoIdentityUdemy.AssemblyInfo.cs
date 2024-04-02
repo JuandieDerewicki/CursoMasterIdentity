f8db8720e9e35fc6d081da73555f054b68016c10
JuandieDerewicki <jdderewicki@gmail.com>
2024-04-02 20:59:32 -0300

    Cambios generales

--- obj/Debug/net6.0/CursoIdentityUdemy.AssemblyInfo.cs
+++ obj/Debug/net6.0/CursoIdentityUdemy.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoIdentityUdemy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc382bac3374cc10b4b771a896280dd3e1719337")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ed10f5b157215c3e3d048a1fa847b2501d96170")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoIdentityUdemy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoIdentityUdemy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
