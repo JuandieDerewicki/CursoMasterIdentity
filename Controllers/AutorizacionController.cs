2321baa5d0d0f6d5c8d3715ea38c2b081b13ee8d
JuandieDerewicki <jdderewicki@gmail.com>
2024-04-04 18:11:41 -0300

    Creamos controlador de Autorizacion y todos sus métodos con sus vistas

---
+++ Controllers/AutorizacionController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CursoIdentityUdemy.Controllers
+{
+    public class AutorizacionController : Controller
+    {
+        public IActionResult AccesoPublico()
+        {
+            return View();
+        }
+
+        public IActionResult AccesoAutenticado()
+        {
+            return View();
+        }
+
+        public IActionResult AccesoUsuario()
+        {
+            return View();
+        }
+
+        public IActionResult AccesoRegistrado()
+        {
+            return View();
+        }
+
+        public IActionResult AccesoAdministrador()
+        {
+            return View();
+        }
+
+        public IActionResult AccesoUsuarioAdministrador()
+        {
+            return View();
+        }
+
+        public IActionResult AccesoUsuarioYAdministrador()
+        {
+            return View();
+        }
+
+        public IActionResult AccesoAdministradorPermisoCrear()
+        {
+            return View();
+        }
+
+        public IActionResult AccesoAdministradorPermisoEditarBorrar()
+        {
+            return View();
+        }
+
+        public IActionResult AccesoAdministradorPermisoCrearEditarBorrar()
+        {
+            return View();
+        }
+    }
+}
+
--- obj/Debug/net6.0/CursoIdentityUdemy.AssemblyInfo.cs
+++ obj/Debug/net6.0/CursoIdentityUdemy.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursoIdentityUdemy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61aefb749d6fb040eb7a7498384f8e0d9ce45ef8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+239d170e1dff7c09e11bee7e8a5b22ef9722b1c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursoIdentityUdemy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursoIdentityUdemy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
